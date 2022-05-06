aa67502e23aa55e90307f2450012a3c42f85919d
sebasnabas <sebasnabas@protonmail.com>
2022-05-06 16:34:03 +0200

    Add machine parameter to setup script

--- tests/integration/testbed_3/setup.sh
+++ tests/integration/testbed_3/setup.sh
@@ -2,13 +2,19 @@
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-vagrant up
+vagrant up "$1"
 
 machine_neighbor_pairs=$(jq -r '.Neighbours | to_entries | .[] | .key as $machine | .value | map($machine + "=" + .) | .[]' network_config.json)
 
 for machine_neighbor_pair in $machine_neighbor_pairs
 do
     machine=${machine_neighbor_pair%=*}
+
+    if [[ "${1:-null}" != null ]] && [[ "$machine" != "$1" ]]
+    then
+        continue
+    fi
+
     neighbour=${machine_neighbor_pair#*=}
     vagrant ssh "$machine" -c "ping -c 1 $neighbour"
 done
