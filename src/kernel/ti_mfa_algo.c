127b6f14a4140cc6c2874fd800f22314e4268e73
sebasnabas <sebasnabas@protonmail.com>
2022-05-15 09:57:16 +0200

    Remove unnecessary include requiring kernel>=5.16

--- src/kernel/ti_mfa_algo.c
+++ src/kernel/ti_mfa_algo.c
@@ -9,7 +9,6 @@
 #include <linux/netdevice.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
-#include <linux/netfilter_netdev.h>
 
 #include <net/arp.h>
 #include <net/neighbour.h>
