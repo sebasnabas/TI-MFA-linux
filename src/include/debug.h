d3fc0568640aaedb37b10aba68f2ec4c8b5c3bf3
sebasnabas <sebasnabas@protonmail.com>
2022-05-25 23:26:54 +0200

    Fix print format

--- src/include/debug.h
+++ src/include/debug.h
@@ -1,9 +1,9 @@
 #ifdef DEBUG
     #include <linux/utsname.h>
-    #define HOST_NAME utsname()->nodename
-#else
-    #define HOST_NAME ""
-#endif
 
-#undef pr_fmt
-#define pr_fmt(fmt) "[%s] %s:%s: " fmt, HOST_NAME, KBUILD_MODNAME, __func__
+    #undef pr_fmt
+    #define pr_fmt(fmt) "[%s] %s:%s: " fmt, utsname()->nodename, KBUILD_MODNAME, __func__
+#else
+    #undef pr_fmt
+    #define pr_fmt(fmt) "%s: " fmt, KBUILD_MODNAME
+#endif
