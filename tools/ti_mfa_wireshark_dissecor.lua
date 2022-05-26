4c7d83025809f0c3ad3c3ab70adb3dea5f3c33e6
sebasnabas <sebasnabas@protonmail.com>
2022-05-26 03:15:08 +0200

    Fix ti-mfa header in wireshark dissector

--- tools/ti_mfa_wireshark_dissecor.lua
+++ tools/ti_mfa_wireshark_dissecor.lua
@@ -1,12 +1,12 @@
 ti_mfa_protocol = Proto("TI-MFA", "TI-MFA Protocol")
 
+f_node_source = ProtoField.ether("TI-MFA.node_source", "Node Source")
 f_link_source = ProtoField.ether("TI-MFA.source", "Link Source")
 f_link_dest = ProtoField.ether("TI-MFA.dest", "Link Destination")
-f_node_source = ProtoField.ether("TI-MFA.node_source", "Node Source")
 f_bos = ProtoField.bool("TI-MFA.bos", "Bottom of Stack")
 
 ti_mfa_protocol.fields = {
-  f_link_source, f_link_dest, f_node_source, f_bos
+  f_node_source, f_link_source, f_link_dest, f_bos
 }
 
 function ti_mfa_protocol.dissector(buffer, pinfo, tree)
@@ -24,9 +24,9 @@ function ti_mfa_protocol.dissector(buffer, pinfo, tree)
   while (not is_bos)
   do
     local subtree = tree:add(ti_mfa_protocol, buffer(), "TI-MFA Link Failure Header")
-    subtree:add(f_link_source, buffer(offset, 6))
+    subtree:add(f_node_source, buffer(offset, 6))
     subtree:add(f_link_dest, buffer(offset + 6, 6))
-    subtree:add(f_node_source, buffer(offset + 12, 6))
+    subtree:add(f_link_source, buffer(offset + 12, 6))
     subtree:add(f_bos, buffer(offset + 18, 1))
 
     is_bos = buffer(offset + 18, 1):uint() == 1
