e9ed1dd383dedc3a1f3289372ac1500b436164d3
sebasnabas <sebasnabas@protonmail.com>
2022-06-12 12:15:06 +0200

    Install backup route on M instead of R

--- tests/integration/testbed_1/run.sh
+++ tests/integration/testbed_1/run.sh
@@ -86,7 +86,7 @@ function test_scenario_2_link_failures {
     vagrant ssh T -c "sudo ip link set $if_T down"
     vagrant ssh R -c "sudo ip link set $if_R down"
 
-    vagrant ssh R -c "ti-mfa-conf add ${link_e_r} 1200 eth1"
+    vagrant ssh M -c "ti-mfa-conf add ${link_e_r} 1200 eth1"
 }
 
 function test_received_packet {
