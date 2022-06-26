8ed478a77b63bd1047ca399a25d8552a9e506bad
sebasnabas <sebasnabas@protonmail.com>
2022-06-26 08:35:56 +0200

    Change hooks to hash table. Add dev register hooks

--- src/kernel/hook.c
+++ src/kernel/hook.c
@@ -1,16 +1,21 @@
 #include "debug.h"
 
 #include <linux/ip.h>
+#include <linux/jhash.h>
+#include <linux/hash.h>
+#include <linux/hashtable.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/net.h>
 #include <linux/netdevice.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/net_namespace.h>
 #include <linux/notifier.h>
 #include <linux/rtnetlink.h>
 #include <linux/socket.h>
 #include <net/mpls.h>
+#include <net/net_namespace.h>
 #include <net/protocol.h>
 #include <net/sock.h>
 
@@ -21,38 +26,16 @@
 
 MODULE_AUTHOR("Sebastian");
 MODULE_DESCRIPTION("Topology Independent Multi-Failure Alternate (TI-MFA)");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 MODULE_LICENSE("GPL");
 MODULE_SOFTDEP("pre: mpls_router");
 
-static struct nf_hook_ops *timfa_hooks;
-static u32 number_of_timfa_hooks;
+#define TABLE_SIZE 5  // 5 bits = 32 entries
+static DEFINE_HASHTABLE(ti_mfa_nf_hook_table, TABLE_SIZE);
 
-static int ti_mfa_notify(struct notifier_block *this, unsigned long event, void *ptr)
-{
-    struct net_device *dev = netdev_notifier_info_to_dev(ptr);
-    struct mpls_dev *mdev;
-
-    mdev = mpls_dev_get(dev);
-    if (!mdev)
-        return NOTIFY_OK;
-
-    switch (event) {
-        case NETDEV_GOING_DOWN:
-            ti_mfa_ifdown(dev);
-            break;
-
-        case NETDEV_UP:
-            ti_mfa_ifup(dev);
-        default:
-            break;
-    }
-
-    return NOTIFY_OK;
-}
-
-static struct notifier_block ti_mfa_dev_notifier = {
-    .notifier_call = ti_mfa_notify,
+struct ti_mfa_nf_hook {
+    struct nf_hook_ops nf_hook;
+    struct hlist_node  hnode;
 };
 
 static unsigned int timfa_ingress_hook(void *priv, struct sk_buff *skb,
@@ -88,6 +71,84 @@ exit:
     return exit_code;
 }
 
+u32 ti_mfa_nf_hook_hash(struct net_device *dev)
+{
+    return jhash(dev->name, IFNAMSIZ, 0);
+}
+
+static int ti_mfa_register_nf_hook(struct net *net, struct net_device *dev)
+{
+    struct ti_mfa_nf_hook *hook;
+
+    hook = kmalloc(sizeof(*hook), GFP_KERNEL);
+    if (hook == NULL) {
+        pr_err("Could not allocate memory for new hook entry\n");
+        return -ENOMEM;
+    }
+    hook->nf_hook.hook = timfa_ingress_hook;
+    hook->nf_hook.hooknum = NF_NETDEV_INGRESS;
+    hook->nf_hook.pf = NFPROTO_NETDEV;
+    hook->nf_hook.priority = NF_IP_PRI_LAST;
+    hook->nf_hook.dev = dev;
+
+    hash_add_rcu(ti_mfa_nf_hook_table, &hook->hnode, ti_mfa_nf_hook_hash(dev));
+
+    return nf_register_net_hook(net, &(hook->nf_hook));
+}
+
+static void ti_mfa_unregister_nf_hook(struct net *net, struct net_device *dev)
+{
+    struct ti_mfa_nf_hook *hook;
+
+    if (dev == NULL || hash_empty(ti_mfa_nf_hook_table)) {
+        return;
+    }
+
+    hash_for_each_possible_rcu(ti_mfa_nf_hook_table, hook,
+                               hnode, ti_mfa_nf_hook_hash(dev)) {
+        pr_debug("Unregistering TI-MFA hook for device: %s!\n", dev->name);
+        nf_unregister_net_hook(net, &(hook->nf_hook));
+    }
+}
+
+static int ti_mfa_notify(struct notifier_block *this, unsigned long event, void *ptr)
+{
+    struct net_device *dev = netdev_notifier_info_to_dev(ptr);
+    struct net *net = dev_net(dev);
+    struct mpls_dev *mdev;
+
+    mdev = mpls_dev_get(dev);
+    if (!mdev || !net) {
+
+        return NOTIFY_OK;
+    }
+
+    switch (event) {
+        case NETDEV_GOING_DOWN:
+            ti_mfa_ifdown(dev);
+            break;
+
+        case NETDEV_UP:
+            ti_mfa_ifup(dev);
+
+        case NETDEV_REGISTER:
+            ti_mfa_register_nf_hook(net, dev);
+            break;
+
+        case NETDEV_UNREGISTER:
+            ti_mfa_unregister_nf_hook(net, dev);
+            break;
+        default:
+            break;
+    }
+
+    return NOTIFY_OK;
+}
+
+static struct notifier_block ti_mfa_dev_notifier = {
+    .notifier_call = ti_mfa_notify,
+};
+
 static int initialize_hooks(void)
 {
     int return_code;
@@ -100,22 +161,12 @@ static int initialize_hooks(void)
 
     pr_debug("Found %d mpls capable net devices\n", number_of_mpls_devices);
 
-    timfa_hooks = kmalloc_array(number_of_mpls_devices * 2, sizeof(struct nf_hook_ops), GFP_KERNEL);
-
     read_lock(&dev_base_lock);
     dev = first_net_device(&init_net);
 
     while (dev)
     {
-        // START Ingress
-        timfa_hooks[i].hook = timfa_ingress_hook;
-        timfa_hooks[i].hooknum = NF_NETDEV_INGRESS;
-        timfa_hooks[i].pf = NFPROTO_NETDEV;
-        timfa_hooks[i].priority = NF_IP_PRI_LAST;
-        timfa_hooks[i].dev = dev;
-
-        return_code = nf_register_net_hook(&init_net, &timfa_hooks[i]);
-
+        return_code = ti_mfa_register_nf_hook(&init_net, dev);
         if (return_code < 0)
         {
             pr_err("Registering ingress hook failed for device %s, with %d\n", dev->name, return_code);
@@ -128,23 +179,19 @@ static int initialize_hooks(void)
         dev = next_net_device(dev);
     }
 
-    pr_debug("Registering %d hooks succeeded\n", number_of_timfa_hooks);
-
 exit:
     read_unlock(&dev_base_lock);
-
-    number_of_timfa_hooks = i;
 
     return return_code;
 }
 
 static void unregister_hooks(void)
 {
-    int i;
-    for (i = 0; i < number_of_timfa_hooks; i++)
-    {
-        pr_debug("Unregistering TI-MFA hook registered on device: %s!\n", timfa_hooks[i].dev->name);
-        nf_unregister_net_hook(&init_net, &timfa_hooks[i]);
+    int i = 0;
+    struct ti_mfa_nf_hook *hook;
+    hash_for_each_rcu(ti_mfa_nf_hook_table, i, hook, hnode) {
+        pr_debug("Unregistering TI-MFA hook registered on device: %s!\n", hook->nf_hook.dev->name);
+        nf_unregister_net_hook(&init_net, &(hook->nf_hook));
     }
 }
 
@@ -154,6 +201,7 @@ static int __init timfa_init(void)
 
     pr_info("TI-MFA started\n");
 
+    hash_init(ti_mfa_nf_hook_table);
     err = initialize_hooks();
     if (err != 0)
         goto out;
@@ -196,7 +244,6 @@ static void __exit timfa_exit(void)
     cleanup_ti_mfa();
     ti_mfa_genl_unregister();
     unregister_hooks();
-    kfree(timfa_hooks);
 
     pr_info("TI-MFA shut down\n");
 }
